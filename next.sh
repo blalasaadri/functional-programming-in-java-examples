55640115ffb6c74ea1fa97fa0e96174cd435a2fc
Alasdair Collinson <blalasaadri@googlemail.com>
2018-11-27 23:16:44 +0100

    Fixed next.sh script

--- next.sh
+++ next.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git stash save -u "Changes in 'immutable datastructures' solutions branch"
-git checkout whenNotToUseThisStuff
+git stash save -u "Changes in 'when not to use this stuff' branch"
+echo "Reached last branch in Devoxx BE presentation"
